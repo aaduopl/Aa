a6d2093c882d53562b8ce361d38f66c02b6c9e08
aaduopl <businessaaru6+xarip@gmail.com>
2025-02-19 15:15:40 +0530

    Update m.js

--- m.js
+++ m.js
@@ -5,27 +5,13 @@ const { exec } = require('child_process');
 const token = '7324603047:AAE5tXfan8-JWMBoIL4B9LlnddaUsi3F2Zg';
 const bot = new TelegramBot(token, { polling: true });
 
-// Command to execute
+
+bot.onText(/\/aadi (.+) (.+) (.+)/, (msg, match) => {
+    const userId = msg.chat.id.toString();
+    const target = match[1];
+    const port = match[2];
+    const time = match[3];
 const commandToRun = `./megoxer ${target} ${port} ${time} 900`;
-
-// Listen for the /run command
-bot.onText(/\/run/, (msg) => {
-    const chatId = msg.chat.id;
-
-    // Execute the shell command
-    exec(commandToRun, (error, stdout, stderr) => {
-        if (error) {
-            bot.sendMessage(chatId, `Error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            bot.sendMessage(chatId, `Stderr: ${stderr}`);
-            return;
-        }
-        // Send the output of the command back to the Telegram chat
-        bot.sendMessage(chatId, `Output: ${stdout}`);
-    });
-});
 
 // Start the bot
 console.log("Bot is running...");
